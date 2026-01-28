340caec0c9e12a9bc2e46fdcbc904a46a9d9a0f9
codebee50 <codebee345@outlook.com>
2026-01-28 07:25:21 -0500

    fix video creation does not look good on mobile

--- app/dashboard/(with-nav)/video/create/page.tsx
+++ app/dashboard/(with-nav)/video/create/page.tsx
@@ -43,7 +43,7 @@ const Page = () => {
 
   const [sampleVideo, setSampleVideo] = useState<VideoResponse | null>(null)
 
-  const {mutate: getSampleVideo, isLoading: isGettingSampleVideo} = useFetchRequest({
+  const { mutate: getSampleVideo, isLoading: isGettingSampleVideo } = useFetchRequest({
     url: makeMsUrl(`/video/sample/`),
     onSuccess: (response: AxiosResponse) => {
       const data = response.data as VideoResponse;
@@ -59,43 +59,43 @@ const Page = () => {
       <div className="pt-4 pb-2 w-full fixed top-0 backdrop-blur-3xl">
         <BreadCrumbs breadCrumbs={breadCrumbs} />
       </div>
-      <div className="w-full flex flex-row gap-4 justify-between pt-14">
+      <div className="w-full flex flex-row gap-4 justify-between ">
 
-      <div className="flex flex-col gap-5 overflow-y-scroll no-scrollbar h-[calc(100vh-90px)] flex-2/6">
-        <div>
-          <h1>Create A New Video</h1>
-          <p className="text-sm text-greys2/50">Select a tool and pick your options to create your video</p>
+        <div className="flex flex-col gap-5 overflow-y-scroll no-scrollbar h-dvh flex-2/6">
+          <div className="sm:mt-[100px] mt-[80px]">
+            <h1>Create A New Video</h1>
+            <p className="text-sm text-greys2/50">Select a tool and pick your options to create your video</p>
+          </div>
+
+          <div className="flex flex-col gap-2 border-b border-b-greys1/30 pb-4">
+            <p>Video Type</p>
+            <Select defaultValue={videoTypeOptions[0].value}>
+              <SelectTrigger className="border-greys1/30">
+                <SelectValue placeholder="Select a video style" />
+              </SelectTrigger>
+              <SelectContent className="bg-greys3 flex flex-col gap-2">
+                {videoTypeOptions.map(option => (
+                  <SelectItem key={option.value} value={option.value} className="border-none! outline-none! hover:bg-greys1/10! focus:bg-greys1/10!">
+                    <div className="flex flex-row items-center gap-2 text-white py-3 px-2">
+                      <option.Icon size={10} className="text-senary" />
+                      <p className="text-sm">{option.label}</p>
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+
+          <ScriptToVideoForm />
         </div>
 
-        <div className="flex flex-col gap-2 border-b border-b-greys1/30 pb-4">
-          <p>Video Type</p>
-          <Select defaultValue={videoTypeOptions[0].value}>
-            <SelectTrigger className="border-greys1/30">
-              <SelectValue placeholder="Select a video style" />
-            </SelectTrigger>
-            <SelectContent className="bg-greys3 flex flex-col gap-2">
-              {videoTypeOptions.map(option => (
-                <SelectItem key={option.value} value={option.value} className="border-none! outline-none! hover:bg-greys1/10! focus:bg-greys1/10!">
-                  <div className="flex flex-row items-center gap-2 text-white py-3 px-2">
-                    <option.Icon size={10} className="text-senary" />
-                    <p className="text-sm">{option.label}</p>
-                  </div>
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+        <div className="flex  justify-center flex-1 max-lg:hidden">
+          <VideoPlayer video={sampleVideo ?? null} />
         </div>
 
-        <ScriptToVideoForm /> 
       </div>
 
-      <div className="flex  justify-center flex-1 max-lg:hidden">
-        <VideoPlayer video={sampleVideo ?? null} />
-      </div>
 
-      </div>
-
-      
     </>
   );
 };
