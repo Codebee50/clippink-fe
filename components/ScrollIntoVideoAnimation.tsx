478fb5b17b8b3e9c1ae0ec822412e21e0ca2c85e
codebee50 <codebee345@outlook.com>
2026-02-12 10:26:52 -0500

    change hero text

--- components/ScrollIntoVideoAnimation.tsx
+++ components/ScrollIntoVideoAnimation.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { useScroll, useTransform, motion } from "framer-motion";
 import { FaPlay } from "react-icons/fa";
 
-const ScrollIntoVideoAnimation = ({ images }: { images: { thumbnail: string, style: string }[] }) => {
+const ScrollIntoVideoAnimation = ({ images }: { images: { thumbnail: string, style: string, videoUrl: string }[] }) => {
     const ref = useRef(null)
 
     const { scrollYProgress } = useScroll({
@@ -21,7 +21,7 @@ const ScrollIntoVideoAnimation = ({ images }: { images: { thumbnail: string, sty
     }, [scrollYProgress]);
     return (
         <div ref={ref} className='max-w-[1300px] mx-auto w-full'>
-          
+
             <div className="slider overflow-hidden w-full flex justify-start padding-x mt-[70px]">
                 <div className="slide-track gap-5 flex flex-row items-center">
                     {[...images, ...images].map((image, index) => (
@@ -32,12 +32,14 @@ const ScrollIntoVideoAnimation = ({ images }: { images: { thumbnail: string, sty
                             <Image src={image.thumbnail} alt={`hero-section-${index}`} width={200} height={200} className="w-full h-full object-cover object-center " />
 
 
-                            <div className='absolute inset-x-0 bottom-0 py-3 px-4 flex flex-row items-center justify-between backdrop-blur-xl'>
-                                <p className='text-white text-sm uppercase'>{image.style}</p>
-                                <div className='bg-white/50 rounded-full sm:p-3 p-2 shrink-0 cursor-pointer'>
-                                    <FaPlay className='text-black max-sm:text-xs' />
+                            <div className='absolute inset-x-0 bottom-0 py-3 px-4 flex flex-row items-center justify-between backdrop-blur-lg'>
+                                <p className='text-white text-sm uppercase tracking-widest'>{image.style}</p>
+                                {
+                                    image.videoUrl && image.videoUrl !== "" && <div className='bg-white/50 rounded-full sm:p-3 p-2 shrink-0 cursor-pointer'>
+                                        <FaPlay className='text-black max-sm:text-xs' />
 
-                                </div>
+                                    </div>
+                                }
                             </div>
 
 
